ce8b0978793491f1506abd0bdf4fbeec42d45767
NjordJ <nj0rd3841@gmail.com>
2022-10-04 12:00:46 +0500

    RecyclerViewAdapter and generate list fun

--- app/src/main/java/com/iruda/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/iruda/recyclerviewexample/MainActivity.kt
@@ -2,10 +2,37 @@ package com.iruda.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val exampleList = generateList(500)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ExampleItemAdapter(exampleList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
     }
+
+    private fun generateList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+
+        for(i in 0 until size){
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_baseline_cake_24
+                1 -> R.drawable.ic_baseline_adb_24
+                else -> R.drawable.ic_baseline_accessibility_24
+            }
+
+            val item = ExampleItem(textOne = "Item $i", textTwo = "Line 2", imageResource = drawable)
+            list += item
+        }
+
+        return list
+    }
+
 }
