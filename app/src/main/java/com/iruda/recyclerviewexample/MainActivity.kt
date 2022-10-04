cd4f5cd76ef0a59da1c7610e2416aeedbd4c15cd
NjordJ <nj0rd3841@gmail.com>
2022-10-04 13:18:25 +0500

    Insert and delete items

--- app/src/main/java/com/iruda/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/iruda/recyclerviewexample/MainActivity.kt
@@ -2,23 +2,49 @@ package com.iruda.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val exampleList = generateList(500)
+    private val adapter = ExampleItemAdapter(exampleList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateList(500)
-
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ExampleItemAdapter(exampleList)
+        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
+
+        val buttonInsert = findViewById<Button>(R.id.button_insert)
+        val buttonDelete = findViewById<Button>(R.id.button_delete)
+
+        buttonInsert.setOnClickListener {
+            val index = Random.nextInt(8)
+
+            val newItem = ExampleItem(
+                R.drawable.ic_baseline_cake_24,
+                "New item at position $index",
+                "Line 2"
+            )
+
+            exampleList.add(index, newItem)
+            adapter.notifyItemInserted(index)
+        }
+
+        buttonDelete.setOnClickListener {
+            val index = Random.nextInt(8)
+
+            exampleList.removeAt(index)
+            adapter.notifyItemRemoved(index)
+        }
     }
 
-    private fun generateList(size: Int): List<ExampleItem> {
+    private fun generateList(size: Int): ArrayList<ExampleItem> {
         val list = ArrayList<ExampleItem>()
 
         for(i in 0 until size){
